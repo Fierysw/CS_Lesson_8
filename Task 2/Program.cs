377635ed28e57f55ef3f66c8617017541dc42ed0
Ваше имя английскими буквами <fieryswv@yandex.ru>
2023-07-17 20:06:10 +0300

    Init commit

---
+++ Task 2/Program.cs
@@ -0,0 +1,56 @@
+﻿// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, 
+// которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт 
+// номер строки с наименьшей суммой элементов: 1 строка
+
+Console.Write("Введите размерность m массива: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите размерность n массива: ");
+int n = int.Parse(Console.ReadLine()!);
+int[,] randomArray = new int[m, n];
+
+void createRandomArray(int m, int n)
+{
+  int i, j;
+  Random rand = new Random();
+  for (i = 0; i < m; i++)
+  {
+    Console.WriteLine();
+    for (j = 0; j < n; j++)
+    {
+      randomArray[i, j] = rand.Next(1, 9);
+      Console.Write($"{randomArray[i, j]} ");
+    }
+  }
+  Console.WriteLine();
+}
+
+int sumRow(int[,] array, int i)
+{
+  int sum = array[i, 0];
+  for (int j = 1; j < array.GetLength(1); j++)
+  {
+    sum += array[i, j];
+  }
+  return sum;
+}
+
+int minSum = 1;
+int sum = sumRow(randomArray, 0);
+for (int i = 1; i < randomArray.GetLength(0); i++)
+{
+  if (sum > sumRow(randomArray, i))
+  {
+    sum = sumRow(randomArray, i);
+    minSum = i + 1;
+  }
+}
+
+Console.WriteLine("\nСозданный массив: ");
+createRandomArray(m, n);
+Console.WriteLine($"\nНаименьшая сумма элементов находится в {minSum} строке");
---
+++ Task 3/Program.cs
@@ -0,0 +1,70 @@
+﻿// Задача 58: Задайте две матрицы. Напишите программу, 
+// которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+
+Console.Write("Введите число строк 1-й матрицы: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число столбцов 1-й матрицы (и строк 2-й): ");
+int n = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число столбцов 2-й матрицы: ");
+int p = int.Parse(Console.ReadLine()!);
+
+int[,] firstMartrix = new int[m, n];
+createRandomArray(firstMartrix);
+Console.WriteLine($"Первая матрица:");
+printArray(firstMartrix);
+
+int[,] secomdMartrix = new int[n, p];
+createRandomArray(secomdMartrix);
+Console.WriteLine($"Вторая матрица:");
+printArray(secomdMartrix);
+
+int[,] resultMatrix = new int[m,p];
+
+multiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"Произведение первой и второй матриц:");
+printArray(resultMatrix);
+
+void multiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
+
+void createRandomArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(9);
+    }
+  }
+}
+
+void printArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write(array[i,j] + " ");
+    }
+    Console.WriteLine();
+  }
+}
